8fe072511c3c61047ce463be76f2d05029978ff6
vishakahegde <hegde.vishaka@gmail.com>
2020-04-29 11:23:47 +0200

    Modified each file, added a new folder called component.Created pokemon.js and worked on components and props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
+
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <p>Javascript is a programming language which makes building apps easier!</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to Codaisseur
-        </a>
-    </div>
+    <main className="container">
+      <Title content="Some Simple Title" />
+      <div className="row">
+        {all_pokemon.map((pokemon) => {
+          return (
+            <div className="col-md-6 col-lg-4">
+              <Pokemon
+                name={pokemon.name}
+                weight={pokemon.weight}
+                awesome={pokemon.awesome}
+                terrifying={pokemon.terrifying}
+                abilities={pokemon.abilities}
+              />
+            </div>
+          );
+        })}
+      </div>
+
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </main>
   );
 }
 
